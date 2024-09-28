a7210a9a2e48be0926b6533a1463257c6e40a360
Szymon Michalak <sz.p.michalak@gmail.com>
2024-09-28 09:16:47 +0200

    Add FloodButton and Location

--- HighWaters/Controllers/ViewController.swift
+++ HighWaters/Controllers/ViewController.swift
@@ -1,11 +1,36 @@
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var floodButton: UIButton! 
+    
+    private lazy var locationManager: CLLocationManager = {
+        
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = kCLDistanceFilterNone
+        manager.requestAlwaysAuthorization()
+        return manager
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
+        setupUI()
+        
+    }
+    
+    private func setupUI() {
+        self.floodButton.layer.cornerRadius = 6.0
+        self.floodButton.layer.masksToBounds = true
     }
 
 
