14770f191aa28eb9b750032217b0a208472ab3a9
Szymon Michalak <sz.p.michalak@gmail.com>
2024-09-28 09:18:24 +0200

    Add annotations to map

--- HighWaters/Controllers/ViewController.swift
+++ HighWaters/Controllers/ViewController.swift
@@ -28,6 +28,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
     }
     
+    @IBAction func addFloodButtonPressed() {
+        
+        guard let location = self.locationManager.location else {
+            return
+        }
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "Flooded"
+        annotation.subtitle = "Reported on 12/10/2018 8:50 AM"
+        annotation.coordinate = location.coordinate
+        
+        self.mapView.addAnnotation(annotation)
+        
+    }
+    
     private func setupUI() {
         self.floodButton.layer.cornerRadius = 6.0
         self.floodButton.layer.masksToBounds = true
