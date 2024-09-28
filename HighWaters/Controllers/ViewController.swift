9fbf03a603fa1321c52f8fafdaf75832ddd58854
Szymon Michalak <sz.p.michalak@gmail.com>
2024-09-28 14:07:34 +0200

    Add cocoapods and firebase

--- HighWaters/Controllers/ViewController.swift
+++ HighWaters/Controllers/ViewController.swift
@@ -23,9 +23,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         self.locationManager.startUpdatingLocation()
         self.mapView.showsUserLocation = true
+        self.mapView.delegate = self
         
         setupUI()
         
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+        let region = MKCoordinateRegion(center: self.mapView.userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.08, longitudeDelta: 0.08))
+        self.mapView.setRegion(region, animated: true)
     }
     
     @IBAction func addFloodButtonPressed() {
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'HighWaters' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks! :linkage => :static
+
+  # Pods for HighWaters
+  ## Add the Firebase pod for Google Analytics
+  pod 'FirebaseAnalytics'
+
+  ## Add the pods for any other Firebase products you want to use in your app
+  pod 'FirebaseAuth'
+  pod 'FirebaseFirestore'
+end
